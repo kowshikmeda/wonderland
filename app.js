c3b6e2a626aa87fd33187e16926e802e34c12152
kowshikmeda <medakowshik8@gmail.com>
2025-03-13 19:51:44 +0530

    ``` No code changes made ```

--- app.js
+++ app.js
@@ -82,7 +82,7 @@ app.use((req,res,next)=>{
 app.use("/listings",listingsRouter);
 app.use("/listings/:id/reviews",reviewsRouter);
 app.get("/", (req, res) => {
-    res.redirect("/listing");
+    res.redirect("/listings");
 });
 
 
